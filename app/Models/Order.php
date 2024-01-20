d02d27942c5bb1c39892d8bd0460a73128f22800
jimmywiraarbaa <jimmywiraarbaa03@gmail.com>
2024-01-20 20:48:24 +0700

    create model

---
+++ app/Models/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'is_paid',
+        'payment_receipt'
+    ];
+
+    public function users(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function products(){
+        return $this->belongsToMany(Product::class);
+    }
+}
