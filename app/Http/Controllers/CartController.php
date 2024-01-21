d6b488239f74381cd31e779f98b0a0e83b6387d4
jimmywiraarbaa <jimmywiraarbaa03@gmail.com>
2024-01-21 23:13:50 +0700

    add to cart

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function add_to_cart(Product $product, Request $request){
+        
+    }
+}
