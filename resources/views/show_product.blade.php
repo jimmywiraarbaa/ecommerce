497d20296e12f8a8d5372c2ecd04897fb3fdaa77
jimmywiraarbaa <jimmywiraarbaa03@gmail.com>
2024-01-21 21:12:02 +0700

    detail product

---
+++ resources/views/show_product.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$product->name}}</title>
+</head>
+<body>
+    <img src="{{url('storage/'. $product->image)}}" alt="" width="100px">
+    <p>{{$product->name}}</p>
+    <p>Rp.{{$product->price}}</p>
+    <p>{{$product->description}}</p>
+    <p>Stock :{{$product->stock}}</p>
+    <a href="{{route('index_product')}}">Kembali</a>
+</body>
+</html>
