2a1cd5cd85a2cb1146e282a0663b77f362f08bf6
jimmywiraarbaa <jimmywiraarbaa03@gmail.com>
2024-01-22 15:26:06 +0700

    add to cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -19,7 +19,7 @@ class Cart extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function product(){a
+    public function product(){
         return $this->belongsTo(Product::class);
     }
 }
