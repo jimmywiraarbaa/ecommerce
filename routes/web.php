b3c72aebe0992bcec670d0139a3255acd4949c98
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-01-22 16:54:03 +0700

    update quantity cart

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,4 @@ Route::delete('/product/{product}', [ProductController::class,'delete_product'])
 
 Route::post('/cart/{product}', [CartController::class,'add_to_cart'])->name('add_to_cart');
 Route::get('/cart', [CartController::class,'show_cart'])->name('show_cart');
+Route::patch('/cart/{cart}', [CartController::class,'update_cart'])->name('update_cart');
