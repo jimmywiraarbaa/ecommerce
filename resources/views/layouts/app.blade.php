8831ad84ec83d24a5749b2c70c646f3110be30bc
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-03-25 22:58:30 +0700

    rapi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,96 +1,97 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-</head>
+        <!-- Scripts -->
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    </head>
 
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ route('index_product') }}">
-                    E-commerce
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ route('index_product') }}">
+                        Opung Waffle Chinatown
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav me-auto">
 
-                    </ul>
+                        </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ms-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                                @if (Route::has('login'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    @if (Auth::user()->is_admin == true)
-                                        <a href="{{ route('create_product') }}" class="dropdown-item">Tambah Produk</a>
-                                    @endif
-                                    <a href="{{ route('index_product') }}" class="dropdown-item">Produk</a>
-                                    @if (!Auth::user()->is_admin == true)
-                                        <a href="{{ route('show_cart') }}" class="dropdown-item">Cart</a>
-                                    @endif
-                                    <a href="{{ route('index_order') }}" class="dropdown-item">Order</a>
-                                    <a href="{{ route('show_profile') }}" class="dropdown-item">Profile</a>
-
-                                    <a class="dropdown-item text-danger" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        @if (Auth::user()->is_admin == true)
+                                            <a href="{{ route('create_product') }}" class="dropdown-item">Tambah Produk</a>
+                                        @endif
+                                        <a href="{{ route('index_product') }}" class="dropdown-item">Produk</a>
+                                        @if (!Auth::user()->is_admin == true)
+                                            <a href="{{ route('show_cart') }}" class="dropdown-item">Cart</a>
+                                        @endif
+                                        <a href="{{ route('index_order') }}" class="dropdown-item">Order</a>
+                                        <a href="{{ route('show_profile') }}" class="dropdown-item">Profile</a>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+                                        <a class="dropdown-item text-danger" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
+    </body>
 
 </html>
--- resources/views/show_product.blade.php
+++ resources/views/show_product.blade.php
@@ -1,65 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $product->name }}</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>{{ $product->name }}</title>
+    </head>
 
-<body>
-    @extends('layouts.app')
+    <body>
+        @extends('layouts.app')
 
-    @section('content')
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">{{ __('Product Detail') }}</div>
+        @section('content')
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="card-header">{{ __('Product Detail') }}</div>
 
-                        <div class="card-body">
-                            <div class="d-flex justify-content-around">
-                                <div class="">
-                                    <img src="{{ url('storage/' . $product->image) }}" alt="" width="200px">
-                                </div>
-                                <div class="">
-                                    <h1>{{ $product->name }}</h1>
-                                    <h3>Rp.{{ $product->price }}</h3>
-                                    <h6>{{ $product->description }}</h6>
-                                    <hr>
-                                    <p>{{ $product->stock }} left</p>
-                                    @if (!Auth::user()->is_admin == true)
-                                        <form action="{{ route('add_to_cart', $product) }}" method="post">
-                                            @csrf
-                                            <div class="input-group mb-3">
-                                                <input class="form-control" type="number" name="amount" value="1"
-                                                    aria-describedby="basic-addon2">
-                                                <div class="input-group-append">
-                                                    <button class="btn btn-outline-primary"
-                                                        type="submit">Keranjang</button>
+                            <div class="card-body">
+                                <div class="d-flex justify-content-around">
+                                    <div class="">
+                                        <img src="{{ url('storage/' . $product->image) }}" alt="" width="200px">
+                                    </div>
+                                    <div class="">
+                                        <h1>{{ $product->name }}</h1>
+                                        <h3>Rp.{{ $product->price }}</h3>
+                                        <h6>{{ $product->description }}</h6>
+                                        <hr>
+                                        <p>{{ $product->stock }} left</p>
+                                        @if (!Auth::user()->is_admin == true)
+                                            <form action="{{ route('add_to_cart', $product) }}" method="post">
+                                                @csrf
+                                                <div class="input-group mb-3">
+                                                    <input class="form-control" type="number" name="amount" value="1"
+                                                        aria-describedby="basic-addon2">
+                                                    <div class="input-group-append">
+                                                        <button class="btn btn-outline-primary"
+                                                            type="submit">Keranjang</button>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </form>
-                                    @endif
-                                    @if (Auth::user()->is_admin == true)
-                                        <form action="{{ route('edit_product', $product) }}" method="get">
-                                            <button class="btn btn-primary" type="submit">Ubah</button>
-                                        </form>
-                                    @endif
-                                    @if ($errors->any())
-                                        @foreach ($errors->all() as $error)
-                                            <p>{{ $error }}</p>
-                                        @endforeach
-                                    @endif
+                                            </form>
+                                        @endif
+                                        @if (Auth::user()->is_admin == true)
+                                            <form action="{{ route('edit_product', $product) }}" method="get">
+                                                <button class="btn btn-primary" type="submit">Ubah</button>
+                                            </form>
+                                        @endif
+                                        @if ($errors->any())
+                                            @foreach ($errors->all() as $error)
+                                                <p>{{ $error }}</p>
+                                            @endforeach
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    @endsection
-</body>
+        @endsection
+    </body>
 
 </html>
